e9ef727c299787274243b0afe069f16cf25a3ad5
Harman <harman.vilva@gmail.com>
2019-11-11 16:48:24 -0500

    Added buttons

--- app/src/main/java/com/example/cstgraphicalinterfaceprogramming/activity_currency_api.java
+++ app/src/main/java/com/example/cstgraphicalinterfaceprogramming/activity_currency_api.java
@@ -1,13 +1,14 @@
 package com.example.cstgraphicalinterfaceprogramming;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_currency_api extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_currency_api);
     }
 }
